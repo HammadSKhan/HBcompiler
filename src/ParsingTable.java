eddd5867eba6d1a9040e06c980156779a45410d7
Hammad-star <hammad.skn1@gmail.com>
2022-12-07 09:53:32 -0600

    Improved parsing table implementation

--- src/ParsingTable.java
+++ src/ParsingTable.java
@@ -1,46 +1,46 @@
 import java.util.Hashtable;
 
 public class ParsingTable {
-    Hashtable<String, String> actionTable = new Hashtable<>();
+    Hashtable<String, ActionReturnValue> actionTable = new Hashtable<>();
     Hashtable<String, Integer> gotoTable = new Hashtable<>();
 
     public ParsingTable() {
-        actionTable.put("0-id", "s5");
-        actionTable.put("0-(", "s4");
-        actionTable.put("1-+", "s6");
-        actionTable.put("1-$", "accept");
-        actionTable.put("2-+", "r2");
-        actionTable.put("2-*", "s7");
-        actionTable.put("2-)", "r2");
-        actionTable.put("2-$", "r2");
-        actionTable.put("3-+", "r4");
-        actionTable.put("3-*", "r4");
-        actionTable.put("3-)", "r4");
-        actionTable.put("3-$", "r4");
-        actionTable.put("4-id", "s5");
-        actionTable.put("4-(", "s4");
-        actionTable.put("5-+", "r6");
-        actionTable.put("5-*", "r6");
-        actionTable.put("5-)", "r6");
-        actionTable.put("5-$", "r6");
-        actionTable.put("6-id", "s5");
-        actionTable.put("6-(", "s4");
-        actionTable.put("7-id", "s5");
-        actionTable.put("7-(", "s4");
-        actionTable.put("8-+", "s6");
-        actionTable.put("8-)", "s11");
-        actionTable.put("9-+", "r1");
-        actionTable.put("9-*", "s7");
-        actionTable.put("9-)", "r1");
-        actionTable.put("9-$", "r1");
-        actionTable.put("10-+", "r3");
-        actionTable.put("10-*", "r3");
-        actionTable.put("10-)", "r3");
-        actionTable.put("10-$", "r3");
-        actionTable.put("11-+", "r5");
-        actionTable.put("11-*", "r5");
-        actionTable.put("11-)", "r5");
-        actionTable.put("11-$", "r5");
+        actionTable.put("0-id", new ActionReturnValue(ParsingAction.shift,5));
+        actionTable.put("0-(", new ActionReturnValue(ParsingAction.shift,4));
+        actionTable.put("1-+", new ActionReturnValue(ParsingAction.shift,6));
+        actionTable.put("1-$", new ActionReturnValue(ParsingAction.accept,-1));
+        actionTable.put("2-+", new ActionReturnValue(ParsingAction.reduce,2));
+        actionTable.put("2-*", new ActionReturnValue(ParsingAction.shift,7));
+        actionTable.put("2-)", new ActionReturnValue(ParsingAction.reduce,2));
+        actionTable.put("2-$", new ActionReturnValue(ParsingAction.reduce,2));
+        actionTable.put("3-+", new ActionReturnValue(ParsingAction.reduce,4));
+        actionTable.put("3-*", new ActionReturnValue(ParsingAction.reduce,4));
+        actionTable.put("3-)", new ActionReturnValue(ParsingAction.reduce,4));
+        actionTable.put("3-$", new ActionReturnValue(ParsingAction.reduce,4));
+        actionTable.put("4-id", new ActionReturnValue(ParsingAction.shift,5));
+        actionTable.put("4-(", new ActionReturnValue(ParsingAction.shift,4));
+        actionTable.put("5-+", new ActionReturnValue(ParsingAction.reduce,6));
+        actionTable.put("5-*", new ActionReturnValue(ParsingAction.reduce,6));
+        actionTable.put("5-)", new ActionReturnValue(ParsingAction.reduce,6));
+        actionTable.put("5-$", new ActionReturnValue(ParsingAction.reduce,6));
+        actionTable.put("6-id", new ActionReturnValue(ParsingAction.shift,5));
+        actionTable.put("6-(", new ActionReturnValue(ParsingAction.shift,4));
+        actionTable.put("7-id", new ActionReturnValue(ParsingAction.shift,5));
+        actionTable.put("7-(", new ActionReturnValue(ParsingAction.shift,4));
+        actionTable.put("8-+", new ActionReturnValue(ParsingAction.shift,6));
+        actionTable.put("8-)", new ActionReturnValue(ParsingAction.shift,11));
+        actionTable.put("9-+", new ActionReturnValue(ParsingAction.reduce,1));
+        actionTable.put("9-*", new ActionReturnValue(ParsingAction.shift,7));
+        actionTable.put("9-)", new ActionReturnValue(ParsingAction.reduce,1));
+        actionTable.put("9-$", new ActionReturnValue(ParsingAction.reduce,1));
+        actionTable.put("10-+", new ActionReturnValue(ParsingAction.reduce,3));
+        actionTable.put("10-*", new ActionReturnValue(ParsingAction.reduce,3));
+        actionTable.put("10-)", new ActionReturnValue(ParsingAction.reduce,3));
+        actionTable.put("10-$", new ActionReturnValue(ParsingAction.reduce,3));
+        actionTable.put("11-+", new ActionReturnValue(ParsingAction.reduce,5));
+        actionTable.put("11-*", new ActionReturnValue(ParsingAction.reduce,5));
+        actionTable.put("11-)", new ActionReturnValue(ParsingAction.reduce,5));
+        actionTable.put("11-$", new ActionReturnValue(ParsingAction.reduce,5));
         //------------------------GOTO----------------//
         gotoTable.put("0-E", 1);
         gotoTable.put("0-T", 2);
@@ -53,7 +53,7 @@ public class ParsingTable {
         gotoTable.put("7-F", 10);
     }
 
-    public String getAction(String state, String terminal) {
+    public ActionReturnValue getAction(String state, String terminal) {
         return actionTable.get(state + "-" + terminal);
     }
 
--- src/SyntacticAnalyzer.java
+++ src/SyntacticAnalyzer.java
@@ -1,15 +1,17 @@
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class SyntacticAnalyzer {
 
     ParsingTable pt = new ParsingTable();
-    SymbolTable st;
-    ArrayList<Token> al;
+    SymbolTable symbolTable;
+    ArrayList<Token> inputBuffer;
     public SyntacticAnalyzer(ArrayList<Token> tokenArrayList,SymbolTable symbolTable) {
-        al = tokenArrayList;
-        st = symbolTable;
+        inputBuffer = tokenArrayList;
+        this.symbolTable = symbolTable;
     }
 
+    Stack<String> inputStack = new Stack<>();
     public String analyze(){
         return "";
     }
